59a67aac6999aab3458a0b8ed39967c2b7de52cc
Michael Haufe <tno@thenewobjective.com>
2020-08-14 15:56:41 -0500

    Added `fix` stub

---
+++ src/fix.ts
@@ -0,0 +1,17 @@
+/*!
+ * @license
+ * Copyright (C) 2020 Michael L Haufe
+ * SPDX-License-Identifier: AGPL-3.0-only
+ * @see <https://spdx.org/licenses/AGPL-3.0-only.html>
+ */
+
+/**
+  *
+  */
+function fix(
+    target: any, _propertyKey: PropertyKey, descriptor: PropertyDescriptor
+): void {
+
+}
+
+export default fix;
